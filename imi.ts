16fddd44aecda49763b7fa6a714c53949d9c92b9
TaggartMaher <taggart.maher@gmail.com>
2024-07-12 08:03:19 -0400

    Added UserLawFirm

---
+++ imi.ts
@@ -0,0 +1,13 @@
+
+export interface UserLawFirm {
+  code: string;
+  law_firm_name: string;
+  street_address_1: string;
+  street_address_2: string;
+  street_address: string;
+  city: string;
+  state: string;
+  zip: string;
+  email: string;
+  phone_number: string;
+}
